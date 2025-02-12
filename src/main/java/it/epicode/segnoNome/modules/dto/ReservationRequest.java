fd63433c9ad7691622cbcbd3e9bf5de054879373
CorinneMainardi <147351705+CorinneMainardi@users.noreply.github.com>
2025-02-12 10:23:22 +0100

    fixato event

--- src/main/java/it/epicode/segnoNome/modules/dto/ReservationRequest.java
+++ src/main/java/it/epicode/segnoNome/modules/dto/ReservationRequest.java
@@ -1,4 +1,13 @@
 package it.epicode.segnoNome.modules.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class ReservationRequest {
+    @NotNull(message = "the field 'user' cannot be null")
+    private Long userId;
+
+    @NotNull(message = "the field 'event' cannot be null")
+    private Long eventId;
 }
