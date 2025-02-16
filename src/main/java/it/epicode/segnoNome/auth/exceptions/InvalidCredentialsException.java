ce3ccd5c011dd99b6694a60a96d6211b5cd2cf49
CorinneMainardi <147351705+CorinneMainardi@users.noreply.github.com>
2025-02-16 17:58:59 +0100

    fixato gestione errori login

---
+++ src/main/java/it/epicode/segnoNome/auth/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package it.epicode.segnoNome.auth.exceptions;
+
+public class InvalidCredentialsException extends RuntimeException {
+  public InvalidCredentialsException(String message) {
+    super(message);
+  }
+}
